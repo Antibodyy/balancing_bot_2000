ca273acd3219cd088a937326a972da9c4acff785
Antibody <94369215+Antibodyy@users.noreply.github.com>
2025-11-28 20:59:27 -0800

    added plotting :D
    idk if its right tho

--- mads.py
+++ mads.py
@@ -4,6 +4,15 @@ import numpy as np
 import mujoco
 import mujoco.viewer
 import scipy.linalg
+import matplotlib.pyplot as plt
+
+phi = np.deg2rad(35.0) 
+data_log = {
+    'time': [],
+    'theta': [],
+    'torque': [],
+    'theta_target': [] 
+}
 
 def get_robot_dynamics():
 
@@ -24,7 +33,6 @@ def get_robot_dynamics():
     L   = 0.2 # Length to COM [m]
     R   = 0.05 # Wheel radius [m]
     g   = 9.81 # Gravity [m/s^2]
-    phi = np.deg2rad(15.0)  # Slope angle [rad]
     W   = 0.3  # Track width (distance between wheels in m)
 
     I_wy = 0.001 # Wheel inertias
@@ -74,13 +82,16 @@ def get_robot_dynamics():
 
 x0 = np.array([0, 0.1, 0, 0, 0, 0])
 u0 = np.array([0.25, 0.25])
+theta_eq = -phi
+x_eq_val = np.array([0, theta_eq, 0, 0, 0, 0])
+u_eq_val = np.zeros(2)
 robot_dynamics, sym_x, sym_u = get_robot_dynamics()
 jac_A = ca.jacobian(robot_dynamics(sym_x, sym_u), sym_x)
 jac_B = ca.jacobian(robot_dynamics(sym_x, sym_u), sym_u)
-A_func = ca.Function('A_func', [sym_x, sym_u], [jac_A])
-B_func = ca.Function('B_func', [sym_x, sym_u], [jac_B])
-A_num = np.array(A_func(np.zeros(6), np.zeros(2)))
-B_num = np.array(B_func(np.zeros(6), np.zeros(2)))
+linearizer = ca.Function('linearizer', [sym_x, sym_u], [jac_A, jac_B])
+A_res, B_res = linearizer(x_eq_val, u_eq_val)
+A_num = np.array(A_res)
+B_num = np.array(B_res)
 Q = np.diag([1.0, 500.0, 1.0, 0.1, 10.0, 0.1]) 
 R = np.eye(2) * 0.1
 P = scipy.linalg.solve_continuous_are(A_num, B_num, Q, R)
@@ -93,17 +104,40 @@ def controller_callback(model, data):
     x_vel = data.qvel[0]
     d_theta = data.qvel[1]
     x_curr = np.array([x_pos, theta, 0.0, x_vel, d_theta, 0.0])
-    u = -K @ x_curr
+    u = -K @ (x_curr-x_eq_val)
     if abs(theta) > 0.05 and int(data.time * 100) % 10 == 0:
         print(f"Tilt: {theta:.3f} rad and LQR Reaction Torque: {u[0]:.3f} N")
     data.ctrl[0] = u[0]
     data.ctrl[1] = u[1]
+    data_log['time'].append(data.time)
+    data_log['theta'].append(theta)
+    data_log['torque'].append(u[0]) # Storing left wheel torque
+    data_log['theta_target'].append(theta_eq)
 
 def main():
     model = mujoco.MjModel.from_xml_path('MEC231A/Final_project/balancing_bot_2000/robot_model.xml')
     data = mujoco.MjData(model)
+    data.qpos[1] = theta_eq
     mujoco.set_mjcb_control(controller_callback)
     mujoco.viewer.launch(model, data)
+    time_arr = np.array(data_log['time'])
+    theta_arr = np.array(data_log['theta'])
+    torque_arr = np.array(data_log['torque'])
+    target_arr = np.array(data_log['theta_target'])
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 8))
+    ax1.plot(time_arr, theta_arr, label='Actual Pitch', color='blue')
+    ax1.plot(time_arr, target_arr, label='Target (Vertical)', color='red', linestyle='--')
+    ax1.set_ylabel('Pitch Angle (rad)')
+    ax1.set_title('Robot Balancing Performance')
+    ax1.legend()
+    ax1.grid(True)
+        
+    ax2.plot(time_arr, torque_arr, label='Motor Torque (Left)', color='green')
+    ax2.set_ylabel('Torque (Nm)')
+    ax2.set_xlabel('Time (s)')
+    ax2.legend()
+    ax2.grid(True)
+    plt.show()
 
 if __name__ == "__main__":
     main()
